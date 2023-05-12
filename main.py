9134afd7410bfa908a8fcb2177170041c49b6a93
Renarin <122590691+Renarin14@users.noreply.github.com>
2023-05-11 23:47:49 -0500

    #update2: creacion de fichas
    cree las 28 fichas con un bucle while usando la clase Ficha

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from fichas import Ficha
 
-ficha1=Ficha(1,2)
-print(ficha1.lado1)
+l=[]
+
+cantidad_fichas=28
+
+contador_final=7
+contador=0
+lado1=0
+contador2=0
+lado2=0
+while(cantidad_fichas!=0):
+    if(contador==contador_final):
+        contador_final-=1
+        lado1+=1
+        contador=0
+        contador2+=1
+        lado2=contador2
+    ficha=Ficha(lado1,lado2)
+    contador+=1
+    cantidad_fichas-=1
+    lado2+=1
+    l.append(ficha)
+
+for i in l:
+    print(i.lado1," - ",i.lado2)
+
