e3c047718990be284f8160d587d5ddf3b407b415
Renarin <122590691+Renarin14@users.noreply.github.com>
2023-05-12 00:24:35 -0500

    update3: creacion clase jugador
    cree la clase jugador
    atributos:
    nombre,lista_fichas
    metodos:
    jugar_ficha,pasar_turno
    notas:
    los metodos estan pendientes de programarlo

---
+++ jugador.py
@@ -0,0 +1,11 @@
+class Jugador:
+    def __init__(self, nombre):
+        self.nombre=nombre
+        self.lista_fichas=[]
+    
+    #Juega la ficha y la elimina de la lista de fichas
+    def jugar_ficha(self, ficha):
+        self.lista_fichas.pop(ficha)
+    #Pasa turno
+    def pasar_turno(self):
+        pass
