a612abb830a05d966752194292b76d4eaa432b75
Renarin <122590691+Renarin14@users.noreply.github.com>
2023-05-11 22:30:00 -0500

    #update1: clase ficha
    cree clase ficha
    atributos:
    -lado1
    -lado2

---
+++ fichas.py
@@ -0,0 +1,7 @@
+class Ficha:
+    def __init__(self, lado1,lado2):
+        self.lado1=lado1
+        self.lado2=lado2
+
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from fichas import Ficha
+
+ficha1=Ficha(1,2)
+print(ficha1.lado1)
