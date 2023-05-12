d2001a49032df2682529326c389d8e9e029b198e
Renarin <122590691+Renarin14@users.noreply.github.com>
2023-05-12 15:28:45 -0500

    Update7: Creacion de primera jugada
    Hice la primera jugada donde se pone el doble 6 como primera jugada,
    se establecen los extremos disponibles para poner fichas y se guarda
    la ficha en la lista de fichas jugadas

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ while(cantidad_fichas!=0):
 for i in range(4):
     player=Jugador()
     if(i==0):
-        player.nombre="Humano"
+        player.nombre="Humano" #Este sera el jugador que nosotros controlemos
     else:
-        player.nombre="Maquina "+str(i)
+        player.nombre="Maquina "+str(i) #Los demas seran maquinas
     mesa.jugadores.append(player)
 """"
 muestra fichas de domino
@@ -43,6 +43,8 @@ for i in mesa.fichas_iniciales:
 
 for i in mesa.jugadores[mesa.jugador_inicial].lista_fichas:
     print(i.numero_ficha," - ",i.lado1," - ",i.lado2)
+
+print(len(mesa.jugadores[jugador_actual].lista_fichas))
 """
 
 #Revolver fichas
@@ -53,6 +55,26 @@ mesa.repartir_fichas()
 
 
 #Inicio del juego
+
+#Este es el primer turno, donde se pone el doble 6 y se pasa turno
+jugador_actual=mesa.jugador_inicial #Designamos al jugador actual que comenzara la partida con el doble 6
+if (jugador_actual==len(mesa.jugadores)):
+    jugador_actual=0
+for i in mesa.jugadores[jugador_actual].lista_fichas:
+    if(i.numero_ficha==28):
+        indice=mesa.jugadores[jugador_actual].lista_fichas.index(i)
+        ficha_jugada=mesa.jugadores[jugador_actual].lista_fichas.pop(indice)
+        
+mesa.fichas_jugadas.append(ficha_jugada) #Agregamos la ficha jugada a la lista de fichas jugadas de mesa
+
+mesa.ficha_extremo1=ficha_jugada.lado1 #Establecemos el extremo 1
+mesa.ficha_extremo2=ficha_jugada.lado2 #Establecemos el extremo 2
+
+
+jugador_actual+=1
+
+  
+    
 
 
 
